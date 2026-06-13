a68328d38a2545453a542437fbcf53e0e63142c1
r9427 <rd.landau@outlook.com>
2026-06-13 10:15:22 +0800

    add config

---
+++ app/common/Config.py
@@ -0,0 +1,18 @@
+import configparser
+import os
+from string import Template
+
+from util.Util import Util
+
+
+class Config(object):
+
+    def __init__(self, config_file_path):
+        parser = configparser.ConfigParser()
+        parser.read_file(config_file_path.open())
+        self.model_qwen_name = Util.strip_string(os.getenv("MODEL_QWEN_NAME", None) or parser['model']['qwen_name'])
+        self.model_qwen_embedding_name = Util.strip_string(os.getenv("MODEL_QWEN_EMBEDDING_NAME", None) or parser['model']['qwen_embedding_name'])
+        self.model_qwen_api_key = Util.strip_string(os.getenv("MODEL_QWEN_API_KEY", None) or parser['model']['qwen_api_key'])
+        self.model_qwen_base_url = Util.strip_string(os.getenv("MODEL_QWEN_BASE_URL", None) or parser['model']['qwen_base_url'])
+
+
