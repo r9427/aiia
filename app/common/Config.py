d06b5836b925d97963edd4b8843756c5ffae3615
r9427 <rd.landau@outlook.com>
2026-06-13 10:23:11 +0800

    add tavily api key config

--- app/common/Config.py
+++ app/common/Config.py
@@ -14,5 +14,6 @@ class Config(object):
         self.model_qwen_embedding_name = Util.strip_string(os.getenv("MODEL_QWEN_EMBEDDING_NAME", None) or parser['model']['qwen_embedding_name'])
         self.model_qwen_api_key = Util.strip_string(os.getenv("MODEL_QWEN_API_KEY", None) or parser['model']['qwen_api_key'])
         self.model_qwen_base_url = Util.strip_string(os.getenv("MODEL_QWEN_BASE_URL", None) or parser['model']['qwen_base_url'])
+        self.tools_tavily_api_key = Util.strip_string(os.getenv("TOOLS_TAVILY_API_KEY", None) or parser['tools']['tavily_api_key'])
 
 
